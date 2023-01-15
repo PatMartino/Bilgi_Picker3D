ea3e4a5842d4af5f6b1b5abfa05b16fa65212b1e
PatMartino <kaan.cuhadar@outlook.com>
2023-01-15 23:24:02 +0300

    Game Finalised

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -45,7 +45,7 @@ namespace Managers
         private void UnSubscribeEvents()
         {
             CameraSignals.Instance.onSetCameraTarget -= OnSetCameraTarget;
-            CoreGameSignals.Instance.onReset += OnReset;
+            CoreGameSignals.Instance.onReset -= OnReset;
         }
 
         private void OnDisable()
